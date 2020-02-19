0c5b8cf029a3f7eff89a75bd585d1ef0e53a9f13
Danilo Portillo <daniloportillo200030@gmail.com>
2020-02-19 13:52:15 -0600

    programa sobre password terminado

--- Inicio_de_sesion.java
+++ Inicio_de_sesion.java
@@ -1,7 +1,19 @@
 package com.curso.progra1;
 
+
+import java.util.Scanner;
+
 public class Inicio_de_sesion {
     public static void main(String[] args) {
-        
+        System.out.print("Ingrese la contrasña correcta: ");
+        final String Palabra_Secreta = "Batman123";
+        Scanner leer_teclado = new Scanner(System.in);
+        String usuario = leer_teclado.nextLine();
+        if (usuario.equals(Palabra_Secreta)){
+            System.out.println("Bienvenido");
+        }
+        else {
+            System.out.println("ERROR");
+        }
     }
 }
