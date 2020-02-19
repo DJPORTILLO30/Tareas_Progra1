ff912fd8b0a0d40189729ac1afa0f2ad88602f8d
Danilo Portillo <daniloportillo200030@gmail.com>
2020-02-19 14:52:18 -0600

    Se creo arreglo con los datos

--- Agenda.java
+++ Agenda.java
@@ -2,6 +2,7 @@ package com.curso.progra1;
 
 public class Agenda {
     public static void main(String[] args) {
-        
+        String agemda[][] = {{"Nombre","Email","Telefono"},{"Danilo Portillo","damiloportillo200030@gmail.com","1234567"}};
+
     }
 }
