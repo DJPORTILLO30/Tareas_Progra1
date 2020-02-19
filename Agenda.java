c8b99ead5b8871f9a325bccdb44e5dabb3430df5
Danilo Portillo <daniloportillo200030@gmail.com>
2020-02-19 15:00:29 -0600

    Impresion de datos de agenda

--- Agenda.java
+++ Agenda.java
@@ -3,6 +3,11 @@ package com.curso.progra1;
 public class Agenda {
     public static void main(String[] args) {
         String agemda[][] = {{"Nombre","Email","Telefono"},{"Danilo Portillo","damiloportillo200030@gmail.com","1234567"}};
-
+        for (int i = 0 ; i<2 ; i++){
+            for (int o = 0 ; o<3 ; o++){
+                System.out.print(agemda[i][o] + " ");
+            }
+            System.out.println();
+        }
     }
 }
